233afa8aa943d482025c6e202417b7caf4e4e80e
liuyq <657791173@qq.com>
2019-06-09 19:57:58 +0800

    轮播完成

--- src/main.js
+++ src/main.js
@@ -3,21 +3,28 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from "fastclick"
+//轮播插件swiper引入
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+
+//是否开发或生产模式
+Vue.config.productionTip = false
 
 
 //静态文件css引入
 import 'styles/reset.css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
 
-//是否开发或生产模式
-Vue.config.productionTip = false
+
+//使用插件
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
+
 
 new Vue({
   el: '#app',
   router,
-  fastClick,
   components: { App },
   template: '<App/>'
 })
